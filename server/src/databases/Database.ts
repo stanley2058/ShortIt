7815cff3d8ec3304f4e458a6bea31fff19c0c1e2
stanley2058 <stanley2058@yahoo.com.tw>
2023-02-17 15:23:26 +0800

    feat: add basic database template

--- server/src/databases/Database.ts
+++ server/src/databases/Database.ts
@@ -1,12 +1,43 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
+import Redis from "ioredis";
+import { createPrismaRedisCache } from "prisma-redis-middleware";
+// apply fixes from https://github.com/Asjas/prisma-redis-middleware/issues/230
+import type TRedis from "prisma-redis-middleware/node_modules/ioredis/built";
+import Env from "../Env";
+import Logger from "../Logger";
 
 export default class Database {
   private static instance?: Database;
   static getInstance = () => this.instance || (this.instance = new this());
 
-  private prisma: PrismaClient;
+  private redis;
+  private prisma;
 
   private constructor() {
-    this.prisma = new PrismaClient();
+    try {
+      this.redis = new Redis(Env.redisUri);
+      Logger.verbose("connected to redis");
+      this.prisma = new PrismaClient();
+      Logger.verbose("connected to postgres");
+
+      const cacheMiddleware: Prisma.Middleware = createPrismaRedisCache({
+        models: [{ model: "ShortUrl", excludeMethods: ["findMany"] }],
+        storage: {
+          type: "redis",
+          options: {
+            client: this.redis as unknown as TRedis,
+            invalidation: { referencesTTL: 300 },
+            log: undefined,
+          },
+        },
+        cacheTime: 300,
+      });
+
+      this.prisma.$use(cacheMiddleware);
+      Logger.verbose("prisma cache middleware attached");
+    } catch (err) {
+      Logger.verbose("", err);
+      Logger.fatal("cannot establish database connection");
+    }
   }
 }
