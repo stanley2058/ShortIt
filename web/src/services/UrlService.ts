ace60c0942f70e8be27f80ec9552804cd29d80c0
stanley2058 <stanley2058@yahoo.com.tw>
2023-02-19 15:38:58 +0800

    feat: wrapper methods for backend APIs

--- web/src/services/UrlService.ts
+++ web/src/services/UrlService.ts
@@ -1,4 +1,25 @@
+import { TOpenGraphUrl } from "../types/TOpenGraphUrl";
+import Envs from "../Envs";
+import { ShortUrl } from "../types/TShortUrl";
+
+type ReqOgUrl = TOpenGraphUrl & { id?: string };
+type ResOgUrl = TOpenGraphUrl & { id: string };
+
 export default class UrlService {
+  private static readonly defaultFetchOption = {
+    credentials: "include",
+    headers: { "Content-Type": "application/json" },
+  } as const;
+  private static readonly error401 = new Error(
+    "please login to perform this operation"
+  );
+  private static readonly error403 = new Error(
+    "user identity does not match target identity"
+  );
+  private static readonly errorUnknown = new Error(
+    "an unknown error has occurred"
+  );
+
   static verifyUrl(url: string): boolean {
     try {
       // eslint-disable-next-line no-new
@@ -6,6 +27,88 @@ export default class UrlService {
       return true;
     } catch (err) {
       return false;
+    }
+  }
+
+  static async postUrl(url: ReqOgUrl): Promise<ResOgUrl> {
+    const res = await fetch(`${Envs.API_URI}/url`, {
+      ...this.defaultFetchOption,
+      method: "POST",
+      body: JSON.stringify(url),
+    });
+
+    switch (res.status) {
+      case 400:
+        throw new Error("please supply the corresponded body parameters");
+      case 401:
+        throw this.error401;
+      case 403:
+        throw this.error403;
+      case 200:
+        return res.json();
+      default:
+        throw this.errorUnknown;
+    }
+  }
+
+  static async getAllUrl(skip?: number, take?: number): Promise<ShortUrl[]> {
+    const requestUrl = new URL(`${Envs.API_URI}/url`);
+    if (skip) requestUrl.searchParams.append("s", skip.toString());
+    if (take) requestUrl.searchParams.append("t", take.toString());
+
+    const res = await fetch(requestUrl, {
+      ...this.defaultFetchOption,
+      method: "GET",
+    });
+
+    switch (res.status) {
+      case 401:
+        throw this.error401;
+      case 200:
+        return res.json();
+      default:
+        throw this.errorUnknown;
+    }
+  }
+
+  static async deleteUrl(id: string): Promise<void> {
+    const res = await fetch(`${Envs.API_URI}/url/${id}`, {
+      ...this.defaultFetchOption,
+      method: "DELETE",
+    });
+
+    switch (res.status) {
+      case 401:
+        throw this.error401;
+      case 403:
+        throw this.error403;
+      case 204:
+        return;
+      default:
+        throw this.errorUnknown;
+    }
+  }
+
+  static async copyToClipboard(text: string): Promise<void> {
+    if (!text) return;
+    const encoded = encodeURI(text);
+    if (!navigator.clipboard) this.fallbackCopyTextToClipboard(encoded);
+    else await navigator.clipboard.writeText(encoded);
+  }
+  private static fallbackCopyTextToClipboard(text: string) {
+    const textArea = document.createElement("textarea");
+    textArea.value = text;
+    // Avoid scrolling to bottom
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+    try {
+      document.execCommand("copy");
+    } finally {
+      document.body.removeChild(textArea);
     }
   }
 }
