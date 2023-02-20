725f6737ce8e79ccd385baf6652a9f03ecda4ffa
stanley2058 <stanley2058@yahoo.com.tw>
2023-02-20 11:43:40 +0800

    fix: rename env to clarify

--- web/src/Envs.ts
+++ web/src/Envs.ts
@@ -1,5 +1,5 @@
 export default class Envs {
-  static readonly API_URL = import.meta.env.VITE_API_URL;
+  static readonly SERVER_URL = import.meta.env.VITE_API_URL;
   static readonly API_VER = import.meta.env.VITE_API_VERSION;
-  static readonly API_URI = `${Envs.API_URL}/api/${Envs.API_VER}`;
+  static readonly API_URI = `${Envs.SERVER_URL}/api/${Envs.API_VER}`;
 }
