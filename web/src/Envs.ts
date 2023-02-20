71346654d5ce43b23b18ed5ca21573db85a1ecb4
stanley2058 <stanley2058@yahoo.com.tw>
2023-02-20 18:23:07 +0800

    fix: use window.origin in PROD

--- web/src/Envs.ts
+++ web/src/Envs.ts
@@ -1,5 +1,7 @@
 export default class Envs {
-  static readonly SERVER_URL = import.meta.env.VITE_API_URL;
+  static readonly SERVER_URL = import.meta.env.PROD
+    ? window.origin
+    : import.meta.env.VITE_API_URL;
   static readonly API_VER = import.meta.env.VITE_API_VERSION;
   static readonly API_URI = `${Envs.SERVER_URL}/api/${Envs.API_VER}`;
 }
