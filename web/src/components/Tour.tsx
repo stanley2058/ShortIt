8d9d4ab1b170482875ad82ed94c5fccd9ea439ec
stanley2058 <stanley2058@yahoo.com.tw>
2023-02-21 18:42:28 +0800

    feat: add dark & light theme for tour

--- web/src/components/Tour.tsx
+++ web/src/components/Tour.tsx
@@ -1,7 +1,46 @@
 import { Title, Text } from "@mantine/core";
-import { Step } from "react-joyride";
+import { Step, Styles } from "react-joyride";
 
-export default function Tour(): Step[] {
+const styleLight: Styles = {
+  beaconInner: {
+    backgroundColor: "#4263eb",
+  },
+  beaconOuter: {
+    borderColor: "#4263eb",
+  },
+  buttonNext: {
+    backgroundColor: "#228be6",
+  },
+};
+const styleDark: Styles = {
+  beaconInner: {
+    backgroundColor: "#4dabf7",
+  },
+  beaconOuter: {
+    borderColor: "#4dabf7",
+  },
+  tooltip: {
+    backgroundColor: "#2C2E33",
+    color: "#c5f6fa",
+  },
+  options: { arrowColor: "#2C2E33" },
+  buttonNext: {
+    backgroundColor: "#228be6",
+    color: "#c5f6fa",
+  },
+  buttonBack: {
+    color: "#c5f6fa",
+  },
+  buttonSkip: {
+    color: "#fd7e14",
+  },
+  buttonClose: {
+    color: "#c5f6fa",
+  },
+};
+
+export default function Tour(theme: "dark" | "light" = "dark"): Step[] {
+  const styles = theme === "dark" ? styleDark : styleLight;
   return [
     {
       content: (
@@ -19,14 +58,17 @@ export default function Tour(): Step[] {
       disableOverlayClose: true,
       placement: "center",
       target: "body",
+      styles,
     },
     {
       content: <Text>Paste your URL here.</Text>,
       target: "#urlInput",
+      styles,
     },
     {
       content: <Text>Press this button to short it!</Text>,
       target: "#submitBtn",
+      styles,
     },
     {
       content: (
@@ -35,23 +77,28 @@ export default function Tour(): Step[] {
         </Text>
       ),
       target: "#customBtn",
+      styles,
     },
     {
       content: <Text>Set your custom options here.</Text>,
       target: "#inputMeta",
+      styles,
     },
     {
       content: <Text>Previews are automatically generated.</Text>,
       target: "#previewMeta",
+      styles,
     },
     {
       content: <Text>Just like this!</Text>,
       target: "#inputForm",
+      styles,
     },
     {
       content: <Text>Press this button to see this tour again.</Text>,
       target: "#btnStartTour",
       locale: { last: "Done!" },
+      styles,
     },
   ];
 }
