68f2331d05b20132559d5fbd71ffda80b636bcb7
stanley2058 <stanley2058@yahoo.com.tw>
2023-02-21 12:07:05 +0800

    fix: assign height

--- web/src/components/Home.tsx
+++ web/src/components/Home.tsx
@@ -3,7 +3,7 @@ import UrlForm from "./UrlForm";
 
 export default function Home() {
   return (
-    <Flex direction="column" justify="center">
+    <Flex h="100%" direction="column" justify="center">
       <Container pb="lg">
         <Text
           variant="gradient"
