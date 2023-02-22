103cb33c973ede583308b8a5212ee6fa0807ddae
stanley2058 <stanley2058@yahoo.com.tw>
2023-02-22 17:43:48 +0800

    ci: use ghcr

--- build-all.sh
+++ build-all.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
 
+REGISTRY="${REGISTRY:=reg.stw.tw}"
+
 pushd "base-image" || exit 1
 ./index.js
-docker pull reg.stw.tw/short-it-base
-if docker images | grep reg.stw.tw/short-it-base &> /dev/null; then
-    docker build -f Dockerfile . -t reg.stw.tw/short-it-base || exit 1
+docker pull "$REGISTRY/short-it-base"
+if docker images | grep "$REGISTRY/short-it-base" &> /dev/null; then
+    docker build -f Dockerfile . -t "$REGISTRY/short-it-base" || exit 1
 else
-    docker build -f Dockerfile.bootstrap . -t reg.stw.tw/short-it-base || exit 1
+    docker build -f Dockerfile.bootstrap . -t "$REGISTRY/short-it-base" || exit 1
 fi
 popd || exit 1
+docker push "$REGISTRY/short-it-base"
 
 pushd "web" || exit 1
-docker build . -t reg.stw.tw/short-it-web || exit 1
+docker build . -t "$REGISTRY/short-it-web" || exit 1
 popd || exit 1
+docker push "$REGISTRY/short-it-web"
 
 pushd "server" || exit 1
-docker build . -t reg.stw.tw/short-it || exit 1
+docker build . -t "$REGISTRY/short-it" || exit 1
 popd || exit 1
-
-docker push reg.stw.tw/short-it-base
-docker push reg.stw.tw/short-it-web
-docker push reg.stw.tw/short-it
+docker push "$REGISTRY/short-it"
