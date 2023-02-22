86d9ee28f26d150a5e00debab640c01e1188f25a
stanley2058 <stanley2058@yahoo.com.tw>
2023-02-22 14:55:24 +0800

    feat: add github action

--- build-all.sh
+++ build-all.sh
@@ -2,19 +2,20 @@
 
 pushd "base-image" || exit 1
 ./index.js
+docker pull reg.stw.tw/short-it-base
 if docker images | grep reg.stw.tw/short-it-base &> /dev/null; then
-    docker build -f Dockerfile . -t reg.stw.tw/short-it-base
+    docker build -f Dockerfile . -t reg.stw.tw/short-it-base || exit 1
 else
-    docker build -f Dockerfile.bootstrap . -t reg.stw.tw/short-it-base
+    docker build -f Dockerfile.bootstrap . -t reg.stw.tw/short-it-base || exit 1
 fi
 popd || exit 1
 
 pushd "web" || exit 1
-docker build . -t reg.stw.tw/short-it-web
+docker build . -t reg.stw.tw/short-it-web || exit 1
 popd || exit 1
 
 pushd "server" || exit 1
-docker build . -t reg.stw.tw/short-it
+docker build . -t reg.stw.tw/short-it || exit 1
 popd || exit 1
 
 docker push reg.stw.tw/short-it-base
