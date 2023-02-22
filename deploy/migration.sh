cc53a211b6b6caa5d86703dd6ec3d55975f8708c
stanley2058 <stanley2058@yahoo.com.tw>
2023-02-22 12:46:06 +0800

    fix: replace docker exec with docker-compose exec

--- deploy/migration.sh
+++ deploy/migration.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-docker exec -it deploy-shortit-1 npm run db:migrate
-docker exec -it deploy-shortit-1 npm run db:push
-
+docker-compose exec shortit npm run db:migrate
+docker-compose exec shortit npm run db:push
