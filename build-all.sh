616851774f0d9c325e14f38b62825101dc6e1be1
stanley2058 <stanley2058@yahoo.com.tw>
2023-03-06 19:23:39 +0800

    fix: allow repo swapping in build script

--- build-all.sh
+++ build-all.sh
@@ -6,19 +6,19 @@ pushd "base-image" || exit 1
 ./index.js
 docker pull "$REGISTRY/short-it-base"
 if docker images | grep "$REGISTRY/short-it-base" &> /dev/null; then
-    docker build -f Dockerfile . -t "$REGISTRY/short-it-base" || exit 1
+    docker build -f Dockerfile . -t "$REGISTRY/short-it-base" --build-arg REPO="${REGISTRY}" || exit 1
 else
-    docker build -f Dockerfile.bootstrap . -t "$REGISTRY/short-it-base" || exit 1
+    docker build -f Dockerfile.bootstrap . -t "$REGISTRY/short-it-base" --build-arg REPO="${REGISTRY}" || exit 1
 fi
 popd || exit 1
 docker push "$REGISTRY/short-it-base"
 
 pushd "web" || exit 1
-docker build . -t "$REGISTRY/short-it-web" || exit 1
+docker build . -t "$REGISTRY/short-it-web" --build-arg REPO="${REGISTRY}" || exit 1
 popd || exit 1
 docker push "$REGISTRY/short-it-web"
 
 pushd "server" || exit 1
-docker build . -t "$REGISTRY/short-it" || exit 1
+docker build . -t "$REGISTRY/short-it" --build-arg REPO="${REGISTRY}" || exit 1
 popd || exit 1
 docker push "$REGISTRY/short-it"
